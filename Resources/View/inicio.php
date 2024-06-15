fbb6a58b71749354bf3ac4ef621433a169a043e8
Jimmy2004S <tucorreo@example.com>
2024-06-15 14:12:23 -0500

    Separar plantilla para los posts del script

--- Resources/View/inicio.php
+++ Resources/View/inicio.php
@@ -1,11 +1,39 @@
 <?php include '../resources/view/template/navegacion.php'; ?>
-        <div class="col-12 container-inicio">
-        </div>
-        <div class="col-12 mt-5">
-                <!-- Mostrar el listado de posts -->
-                <div class="container mt-2">
-                    <div class="row w-80" id="all-posts">
-                    </div>
+<div class="col-12 container-inicio">
+</div>
+<div class="col-12 mt-5">
+        <div class="container mt-2">
+                <div class="row w-80" id="all-posts">
+                        <!-- Aquí se llenará dinámicamente la lista de posts -->
                 </div>
         </div>
+
+        <!-- Plantilla de post oculta -->
+        <div id="templates" style="display: none;">
+                <div id="post-template">
+                        <div class="col-sm-12 col-md-6">
+                                <div post-id="{{id}}" class="shadow-lg mb-5 bg-body-tertiary rounded card border-primary mb-3" style="height: 90%;">
+                                        <div class="card-header">
+                                                <h4 class="card-title">{{title}}</h4>
+                                                <h6 class="card-subtitle text-muted">{{created_at}}</h6>
+                                        </div>
+                                        <img src="https://picsum.photos/400/200" alt="" />
+                                        <div class="card-body">
+                                                <p class="card-text">{{description}}</p>
+                                                <a href="#" target="blank" class="card-link">Link 1</a>
+                                                <a href="#" class="card-link">Link 2</a>
+                                        </div>
+                                        <div class="card-footer text-muted d-flex">
+                                                <div class="col-10">
+                                                        <p></p>
+                                                </div>
+                                                <div class="col-2">
+                                                        <p class="m-0"></p>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+</div>
 <?php include '../resources/view/template/footer.php'; ?>
--- public/js/app.js
+++ public/js/app.js
@@ -11,34 +11,18 @@ function listarPosts() {
       let posts = JSON.parse(response);
       console.log("parseado", posts);
       let template = "";
+      let postTemplate = $("#post-template").html(); // Obtener la plantilla desde el elemento oculto
       posts.forEach((element) => {
-        template += `
-        <div class="col-sm-12 col-md-6">
-            <div post-id="${element.id}" class="shadow-lg mb-5 bg-body-tertiary rounded card border-primary mb-3"
-            style="height: 90%;">
-                <div class="card-header">
-                    <h4 class="card-title">${element.title}</h4>
-                    <h6 class="card-subtitle text-muted">${element.created_at}</h6>
-                </div>
-                <img src="https://picsum.photos/400/200" alt="" />
-                <div class="card-body">
-                    <p class="card-text">${element.description}</p>
-                    <a href="#" target="blank" class="card-link">Link 1</a>
-                    <a href="#" class="card-link">Link 2</a>
-                </div>
-                <div class="card-footer text-muted d-flex">
-                    <div class="col-10">
-                        <p></p>
-                    </div>
-                    <div  class="col-2">
-                        <p class="m-0"></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-        $("#all-posts").html(template);
+        // Reemplazar las variables en la plantilla con los datos del post
+        let postHTML = postTemplate
+          .replace("{{id}}", element.id)
+          .replace("{{title}}", element.title)
+          .replace("{{created_at}}", element.created_at)
+          .replace("{{description}}", element.description);
+
+        template += postHTML;
       });
+      $("#all-posts").html(template);
     },
     error: function (error) {
       console.log(error);
