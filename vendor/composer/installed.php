d51209a58dbc66a63878015de331a1ba69771ee8
Jimmy2004S <tucorreo@example.com>
2024-06-28 11:13:17 -0500

    Instalacion de dependencias --composer-update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ccf5fbdf027de177f8be485ad89d14da31c79fd4',
+        'reference' => '557735dda8b31b349fc64e728b9c299b59ca178a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ccf5fbdf027de177f8be485ad89d14da31c79fd4',
+            'reference' => '557735dda8b31b349fc64e728b9c299b59ca178a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
