3265d65fb1eff7a9132d6e2197ea761457236c9c
Jimmy2004S <tucorreo@example.com>
2024-07-16 15:12:32 -0500

    Backend: conexion ala BD estatica --refactorizacion

--- DataBase/Config/DB.php
+++ DataBase/Config/DB.php
@@ -6,21 +6,29 @@ class DB{
     private $host;
     private $password;
     private $user;
-    function __construct(){
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct()
+    {
         $this->schema = $_ENV['DATABASE'];
         $this->host = $_ENV['DB_HOST'];
         $this->password = $_ENV['DB_PASS'];
         $this->user = $_ENV['DB_USER'];
+
+        try {
+            $this->pdo = new \PDO("mysql:host=$this->host;dbname=$this->schema", $this->user, $this->password);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            throw new \Exception("No se pudo conectar a la base de datos: " . $e->getMessage());
+        }
     }
 
-    protected function Connection(){
-        try{
-            $conexion = new \PDO("mysql:host=$this->host;dbname=$this->schema", $this->user, $this->password);
-            if($conexion){
-                return $conexion;
-            }
-        }catch(\PDOException $e){
-            return "No se pudo conectar ala base de datos: $e-> getMessage()";
+    public static function getConnection()
+    {
+        if (!self::$instance) {
+            self::$instance = new DB();
         }
+        return self::$instance->pdo;
     }
 }
