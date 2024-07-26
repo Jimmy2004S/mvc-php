63e037f2a4858d94b9cb956a750ac75688304314
Jimmy2004S <tucorreo@example.com>
2024-07-26 17:34:14 -0500

    Backend: manejo de excepcion en subida de archivo

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once '../Lib/App.php';
+require_once __DIR__ . '/../Lib/App.php';
+require_once __DIR__ . '/../App/Exception/handler.php';
+
+// Asegúrate de que la función esté definida antes de llamarla
+set_error_handler('App\Exception\exception_error_handler');
+
 use Dotenv\Dotenv;
 
 // Cargar variables de entorno desde el archivo .env
